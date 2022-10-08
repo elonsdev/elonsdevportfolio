5052526317575e9d6c8be3f8cc97f9a90aecaafb
elonsdev.eth <83188557+elonsdev@users.noreply.github.com>
2022-10-08 16:18:48 +0200

    completed for now...
    complete portfolio and cv.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  darkMode: 'class',
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        PressStart2P:'PressStart2P',
+      },
+      backgroundImage: {
+        'space': "url('../public/sun_moon.png')",
+      }
+    },
   },
   plugins: [],
 }
