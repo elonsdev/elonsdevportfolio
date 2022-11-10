37b94b13a95e7334df156aadee190e4e3babdf09
elonsdev.eth <83188557+elonsdev@users.noreply.github.com>
2022-11-10 19:10:43 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -142,7 +142,7 @@ export default function Home() {
           <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap'>
             <div className='basis-1/3 flex-1'>
               <Image
-                src={web7}
+                src={web9}
                 className='rounded-lg object-cover'
                 width={"100%"}
                 height={"100%"}
@@ -162,7 +162,7 @@ export default function Home() {
             </div>
             <div className='basis-1/3 flex-1'>
               <Image
-                src={web9}
+                src={web10}
                 className='rounded-lg object-cover'
                 width={"100%"}
                 height={"100%"}
@@ -182,7 +182,7 @@ export default function Home() {
             </div>
             <div className='basis-1/3 flex-1'>
               <Image
-                src={web10}
+                src={web7}
                 className='rounded-lg object-cover'
                 width={"100%"}
                 height={"100%"}
